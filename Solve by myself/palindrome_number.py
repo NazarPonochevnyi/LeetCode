33f8edf7c9b8a326bebed8f5e3c6d1a02479e44b
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2018-03-29 14:03:15 +0300

    Create palindrome_number.py

---
+++ Solve by myself/palindrome_number.py
@@ -0,0 +1,15 @@
+def isPalindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0: return False
+    s_x = str(x)
+    len_x = len(s_x)
+    for i in range(1, len_x):
+        if s_x[i - 1] != s_x[-1 * i]:
+            return False
+    return True
+    
+number = -27896532
+print(isPalindrome(number))
